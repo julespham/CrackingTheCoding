88b18a8e704bf2f4c81064534d1da614b33a4dbe
jp93816 <jpham120995@gmail.com>
2020-01-15 22:44:35 -0500

    Add myqueue

--- StacksAndQueues/MyQueue.java
+++ StacksAndQueues/MyQueue.java
@@ -2,12 +2,44 @@ public class MyQueue {
     Stack stack1;
     Stack stack2;
 
-    public Queue(int capacity) {
+    public MyQueue(int capacity) {
         stack1 = new Stack(capacity);
         stack2 = new Stack(capacity);
     }
 
-    public void add() {
-        
+    /**
+     * Adds an element to the queue
+     */
+    public void enqueue(int element) {
+        stack1.push(element);
     }
+
+    /**
+     * Removes an element from the queue 
+     * @return an integer from the queue
+     */
+    public int dequeue() {
+        if (isEmpty(stack2)) {
+            while (!isEmpty(stack1)) {
+                stack2.push(stack1.pop());
+            }
+        }s
+        return stack2.pop();
+    }
+    /**
+     * default isEmpty() method
+     */
+    public boolean isEmpty() {
+        return stack2.size == 0;
+    }
+
+    /**
+     * 
+     * @param s
+     * @return
+     */
+    public boolean isEmpty(Stack s) {
+        return s.size == 0;
+    }
+    
 }
